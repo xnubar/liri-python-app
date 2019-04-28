34a9928d7ea2d2fb275da81150d580e6f9760ce4
xnubar <nxalidova@std.qu.edu.az>
2019-04-28 12:17:33 +0100

    initial commit

---
+++ keys.py
@@ -0,0 +1,17 @@
+impot os
+from dotenv import load_dotenv
+load_dotenv()
+
+print("loaded")
+
+twitter = {
+  "consumer_key": os.getenv("TWITTER_CONSUMER_KEY"),
+  "consumer_secret": os.getenv("TWITTER_CONSUMER_SECRET"),
+  "access_token_key": os.getenv("TWITTER_ACCESS_TOKEN_KEY"),
+  "access_token_secret": os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+}
+
+spotify = {
+  "id": os.getenv("SPOTIFY_ID"),
+  "secret": os.getenv("SPOTIFY_SECRET")
+}
